7d5324774c2466b609b54099e3bd00604aa4af74
JavierInuzuka <javierinuzuka@gmail.com>
2016-11-17 11:31:11 -0300

    Mmm

--- Assets/phps/logout.php
+++ Assets/phps/logout.php
@@ -1,8 +1,16 @@
 <?php
-//verificamos si la sesion esta iniciada
+/**
+* Pagina de cierre de sesion
+*
+*En esta pagina se realiza el cierre de sesion en el sistema, luego de
+*llevarse a cabo, el usuario es redireccionado a la pagina de Login
+*@author Wiccan
+*@param Esta funcion no pide parametros
+*@return Esta funcion no retorna nada, pero redirreciona a otra pagina
+*/
 @session_start();
 //realizamos la desconexion del servidor
 session_destroy();
-//redireccionamos 
+//redireccionamos
 header("location: ../../Login.html")
  ?>
