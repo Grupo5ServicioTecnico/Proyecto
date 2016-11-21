24a73a0c0ef6e83fabe43097b753bc0778675dfe
SilverCrown <chr.a.gomez.m@gmail.com>
2016-11-21 09:57:54 -0300

    update
    arreglo boton buscar emple

--- Assets/phps/buscar_empleado.php
+++ Assets/phps/buscar_empleado.php
@@ -16,40 +16,46 @@
 */
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+if(isset($_REQUEST['search_empl'])) {
+    require_once("config/db.conf.php");
+    $db = "host=$host port=$port dbname=$dbname user=$username password=$password";
+    $cnx = pg_connect($db) or die ('connection failed'. pg_last_error());
+    session_start();
 
-require_once("config/db.conf.php");
-$db = "host=$host port=$port dbname=$dbname user=$username password=$password";
-$cnx = pg_connect($db) or die ('connection failed'. pg_last_error());
-session_start();
+    /**
+    * Importar los daros del cliente
+    * Realizar consulta de busqueda
+    * Consultar la cantidad de filas afectadas
+    * Pasamos los resultados requeridos a variables
+    * Mostramos los datos en la pagina
+    */
 
-/**
-* Importar los daros del cliente
-* Realizar consulta de busqueda
-* Consultar la cantidad de filas afectadas
-* Pasamos los resultados requeridos a variables
-* Mostramos los datos en la pagina
-*/
+    $user = htmlentities($_REQUEST["search_empl"], ENT_QUOTES);
 
-$user = htmlentities($_POST["search_empl"], ENT_QUOTES);
+    $result = pg_query('SELECT * FROM usuarios, empleados, roles  WHERE usuarios.usu_pk=empleados.usu_fk AND empleados.rol_fk= roles.rol_pk AND usu_rut=\''.$user.'\'');
 
-$result = pg_query('SELECT * FROM usuarios, empleados, roles  WHERE usuarios.usu_pk=empleados.usu_fk AND empleados.rol_fk= roles.rol_pk AND usu_rut=\''.$user.'\'');
+    $registros= pg_num_rows($result);
+    $empl = array();
+    for ($i=0;$i<$registros;$i++){
+      $row = pg_fetch_array ( $result,$i );
+      $empl = array(
+        'id' => $row["emp_pk"],
+        'rut' => $row["usu_rut"],
+        'nombre' => $row["usu_nombre"],
+        'apellido' => $row["usu_apellido"],
+        'telefono' => $row["usu_telefono"],
+        'correo' => $row["usu_correo"],
+        'cargo' => $row["rol_nombre"]
+      );
+    }
+    if($empl) {
+        echo json_encode($empl);
+    } else {
+        echo "no";
+    }
+    return $result;
+    pg_free_result($result);
+    pg_close();
+} else
 
-$registros= pg_num_rows($result);
-
-for ($i=0;$i<$registros;$i++){
-  $row = pg_fetch_array ( $result,$i );
-  $empl = array(
-    'id' => $row["emp_id"],
-    'rut' => $row["usu_rut"],
-    'nombre' => $row["usu_nombre"],
-    'apellido' => $row["usu_apellido"],
-    'telefono' => $row["usu_telefono"],
-    'correo' => $row["usu_correo"],
-    'cargo' => $row["rol_nombre"]
-  );
-}
-echo json_encode($empl);
-return $result;
-pg_free_result($result);
-pg_close();
 ?>
--- Assets/phps/producto_add.php
+++ Assets/phps/producto_add.php
@@ -1,4 +1,5 @@
 <?php
+/*Verificacion de Sesion*/
 session_start();
 if (!isset($_SESSION["k_username"])) {
 	header('Location:../../Login.html');
@@ -7,44 +8,58 @@ if (!isset($_SESSION["k_username"])) {
 ?>
 <html>
 	<head>
+		<!--Dentro del HEAD argamos css y nombramos pagina -->
 		<meta charset="utf-8" />
 		<title>Ingreso Producto</title>
 		<link rel="stylesheet" href="../css/formalize.css">
 	</head>
-	<body class="formulario">
+	<body>
+		<!--Cargamos barra de vinculos -->
 		<OBJECT  data="../../barra.html" width="100%" height="50px" border="2px">
 		</object>
+		<!--Cabezera - Contiene Formulario -->
 		<div id="cabezera">
-		<form align="center" action="producto_add2.php" method="post">
-			<h2>Ingreso Producto </h2>
-			<div class="u-form-group" title="(con puntos y guion)">
-					<label >Rut Cliente</label>
-					<input id="rut" size="12" type="rut" name="rut" min="12" max="12"required/>
-					<div id="msgUsuario"></div>
-			</div>
-		  <script type="text/javascript" src="../../Assets/js/jquery-1.12.4.min.js">
-			</script>
-			<script type="text/javascript">
-			  $('#rut').blur( function(){
-			    if($('#rut').val()!= ""){
-			        $.ajax({
-			            type: "POST",
-			            url: "validar.php",
-			            data: "rut="+$('#rut').val(),
-			            beforeSend: function(){
-			              $('#msgUsuario').html('<img src="../images/loader.gif"/> verificando');
-			            },
-			            success: function( respuesta ){
-			              console.log(respuesta);
-			              if(respuesta == '0')
-			                $('#msgUsuario').html('<img src="../images/Ok.png"/>');
-			              else
-			                $('#msgUsuario').html('<img src="../images/Cancel.png"/> <a href="cliente_add.php"><img src="../images/Add.png"/></a>');
-			            }
-			        });
-			    }
-				});
-			</script>
-		<p class="frmd">
+			<form align="center" action="producto_add2.php" method="post">
+				<h2>Ingreso Producto </h2>
+				<table align="center">
+					<tr>
+						<td>
+							<input id="rut" placeholder="Cliente Ej:11 111 111-1"size="20" type="rut" name="rut" min="12" max="12" required>
+						</td>
+						<td>
+							<div id="msgUsuario"></div>
+						</td>
+					</tr>
+					<tr>
+						
+					</tr>
+				</table>
+
+
+			</form>
+		</div>
+		<script type="text/javascript" src="../../Assets/js/jquery-1.12.4.min.js">
+		</script>
+		<script type="text/javascript">
+			$('#rut').blur( function(){
+			  if($('#rut').val()!= ""){
+		        $.ajax({
+	            type: "POST",
+		          url: "validar.php",
+	            data: "rut="+$('#rut').val(),
+	            beforeSend: function(){
+	              $('#msgUsuario').html('<img src="../images/loader.gif"/> verificando');
+		          },
+			        success: function( respuesta ){
+			          console.log(respuesta);
+			          if(respuesta == '0')
+			            $('#msgUsuario').html('<img src="../images/Ok.png"/>');
+			          else
+			            $('#msgUsuario').html('<img src="../images/Cancel.png"/> <a href="cliente_add.php"><img src="../images/Add.png"/></a>');
+			          }
+			      });
+			  }
+			});
+		</script>
 	</body>
 </html>
